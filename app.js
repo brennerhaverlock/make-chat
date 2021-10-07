090a59199218eb9be722d79c65f9ca78a120d04f
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-07 16:02:16 -0700

    Users can log in

--- app.js
+++ app.js
@@ -2,20 +2,27 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const exphbs  = require('express-handlebars');
+var handlebars = require('express-handlebars').create({extname: '.hbs'});
+
+//Express View Engine for Handlebars
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+//Establish your public folder
+app.use('/public/views', express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('main.handlebars');
+})
 
 //Socket.io
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
-  console.log("New user connected! 🔌");
+  console.log("🔌 New user connected! 🔌");
+  require('./sockets/chat.js')(io, socket);
 })
 
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-  res.render('index.handlebars');
-})
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
